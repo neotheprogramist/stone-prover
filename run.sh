be811b3f3b35dc0b8dc010272b2e7092de74670d
Neo <neotheprogramist@proton.me>
2024-03-13 11:15:18 +0000

    scripts enhancements

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ print_in_blue() {
 }
 
 print_in_blue "Creating python virtual environment..." && \
-time ./scripts/create-venv.sh && \
+time ./scripts/venv.sh && \
 
 print_in_blue "Building the prover..." && \
 time ./scripts/build.sh && \
